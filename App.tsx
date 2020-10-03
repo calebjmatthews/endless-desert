d375a9e32a5425b480d87c4a5fd1bd408313f3af
Caleb Matthews <calebjmatthews@gmail.com>
2020-10-03 14:15:49 -0500

    Switchable tag JSX variable

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import Buildings from './components/buildings';
+import BuildingsComponent from './components/buildings';
 import { styles } from './styles';
 
 import Hourglass from './models/hourglass';
@@ -10,6 +10,9 @@ import { buildingsStarting } from './instances/buildings_starting';
 
 export default function App() {
   const [lastTimestamp, setLastTimestamp] = useState(new Date(Date.now()).valueOf());
+  const [selectedTab, selectTab] = useState('Buildings');
+  let tab = <BuildingsComponent buildings={buildingsStarting} />;
+
   useEffect(() => {
     const timeout = setTimeout(() => {
       const hourglass = new Hourglass();
@@ -18,11 +21,19 @@ export default function App() {
     }, 100);
   }, [lastTimestamp]);
 
+  useEffect(() => {
+    switch(selectedTab) {
+      case 'Buildings':
+      tab = <BuildingsComponent buildings={buildingsStarting} />;
+      break;
+    }
+  }, [selectedTab])
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.statusBarSpacer}></View>
-      <Buildings />
+      {tab}
     </View>
   );
 }
